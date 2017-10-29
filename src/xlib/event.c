uTox/uTox

Fix webcam issues on XLIB
closes #873

src/xlib/event.c
@@ -276,19 +276,12 @@ bool doevent(XEvent event) {
         if (event.type == ClientMessage) {
             XClientMessageEvent *ev = &event.xclient;
             if ((Atom)event.xclient.data.l[0] == wm_delete_window) {
-                if (ev->window == video_win[0]) {
-                    postmessage_utoxav(UTOXAV_STOP_VIDEO, 1, 0, NULL);
+                uint32_t r = brtfrs_video_windows(ev->window);
+                if (r == UINT32_MAX) {
                     return true;
                 }
 
-                uint32_t i;
-                for (i = 0; i != self.friend_list_count; i++) {
-                    if (video_win[i + 1] == ev->window) {
-                        FRIEND *f = get_friend(i);
-                        postmessage_utoxav(UTOXAV_STOP_VIDEO, f->number, 0, NULL);
-                        break;
-                    }
-                }
+                postmessage_utoxav(UTOXAV_STOP_VIDEO, r, (r == UINT16_MAX), NULL);
             }
         }
 

src/xlib/main.h
@@ -48,7 +48,6 @@ uint8_t pointergrab;
 
 bool     _redraw;
 
-Window  video_win[32]; // TODO we should allocate this dynamically but this'll work for now
 XImage *screen_image;
 
 extern int utox_v4l_fd;
@@ -83,6 +82,10 @@ void setclipboard(void);
 void pastebestformat(const Atom atoms[], size_t len, Atom selection);
 void formaturilist(char *out, const char *in, size_t len);
 void pastedata(void *data, Atom type, size_t len, bool select);
+
+// Brute Force, the video window we got a close command on (xlib/video.c)
+uint32_t brtfrs_video_windows(Window w);
+
 
 // video4linux
 bool v4l_init(char *dev_name);

timofonic-comms/uToxx

src/xlib/event.c
@@ -264,18 +264,12 @@ bool doevent(XEvent event) {
         if (event.type == ClientMessage) {
             XClientMessageEvent *ev = &event.xclient;
             if ((Atom)event.xclient.data.l[0] == wm_delete_window) {
-                if (ev->window == video_win[0]) {
-                    postmessage_utoxav(UTOXAV_STOP_VIDEO, 1, 0, NULL);
+                uint32_t r = find_video_windows(ev->window);
+                if (r == UINT32_MAX) {
                     return true;
                 }
 
-                for (uint32_t i = 0; i != self.friend_list_count; i++) {
-                    if (video_win[i + 1] == ev->window) {
-                        FRIEND *f = get_friend(i);
-                        postmessage_utoxav(UTOXAV_STOP_VIDEO, f->number, 0, NULL);
-                        break;
-                    }
-                }
+                postmessage_utoxav(UTOXAV_STOP_VIDEO, r, (r == UINT16_MAX), NULL);
             }
         }
 

src/xlib/main.h
@@ -48,7 +48,6 @@ uint8_t pointergrab;
 
 bool     _redraw;
 
-Window  video_win[32]; // TODO we should allocate this dynamically but this'll work for now
 XImage *screen_image;
 
 extern int utox_v4l_fd;
@@ -83,6 +82,10 @@ void setclipboard(void);
 void pastebestformat(const Atom atoms[], size_t len, Atom selection);
 void formaturilist(char *out, const char *in, size_t len);
 void pastedata(void *data, Atom type, size_t len, bool select);
+
+// Brute Force, the video window we got a close command on (xlib/video.c)
+uint16_t find_video_windows(Window w);
+
 
 // video4linux
 bool v4l_init(char *dev_name);